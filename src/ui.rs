190bb00a2de9c5362f5644e17e5398e938fcb79d
Tai Zeming <tai.zeming@pithos.net>
2024-09-26 23:03:08 +0800

    chore: remove relm macro

--- src/ui.rs
+++ src/ui.rs
@@ -1,6 +1,8 @@
-use gtk::prelude::{BoxExt, ButtonExt, OrientableExt};
+use gtk::glib::clone;
+use gtk::prelude::{BoxExt, ButtonExt, GtkWindowExt};
 use gtk4_layer_shell::{Edge, Layer, LayerShell};
 use relm4::{gtk, ComponentParts, ComponentSender, RelmWidgetExt, SimpleComponent};
+
 use tracing::info;
 
 use crate::service::KeyboardHandle;
@@ -15,54 +17,100 @@ pub enum UIMessage {
     Release,
 }
 
-#[relm4::component(pub)]
 impl SimpleComponent for UIModel {
     type Init = Box<dyn KeyboardHandle>;
 
     type Input = UIMessage;
     type Output = ();
+    type Root = gtk::Window;
+    type Widgets = ();
 
-    view! {
-        gtk::Window {
-            init_layer_shell: (),
-            set_layer: Layer::Overlay,
-
-            set_anchor: (Edge::Left, true),
-            set_anchor: (Edge::Right, true),
-            set_anchor: (Edge::Top, false),
-            set_anchor: (Edge::Bottom, true),
-
-
-
-            gtk::Box {
-                set_orientation: gtk::Orientation::Vertical,
-                set_spacing: 5,
-                set_margin_all: 5,
-
-                gtk::Button {
-                    set_label: "Key Press",
-                    connect_clicked => UIMessage::Press
-                },
-
-                gtk::Button::with_label("Key Release") {
-                    connect_clicked => UIMessage::Release
-                },
-            }
-        }
+    fn init_root() -> Self::Root {
+        gtk::Window::builder()
+            .title("Simple app")
+            .default_width(300)
+            .default_height(100)
+            .build()
     }
+
+    // view! {
+    //     gtk::Window {
+    //         init_layer_shell: (),
+    //         set_layer: Layer::Overlay,
+
+    //         set_anchor: (Edge::Left, true),
+    //         set_anchor: (Edge::Right, true),
+    //         set_anchor: (Edge::Top, false),
+    //         set_anchor: (Edge::Bottom, true),
+
+    //         gtk::Box {
+    //             set_orientation: gtk::Orientation::Horizontal,
+    //             set_valign(gtk::Align::Center),
+    //             set_spacing: 5,
+    //             set_margin_all: 5,
+
+    //             gtk::Button {
+    //                 set_label: "Press",
+    //                 connect_clicked => UIMessage::Press
+
+    //             },
+
+    //             gtk::Button::with_label("Release") {
+    //                 connect_clicked => UIMessage::Release
+    //             },
+    //         }
+    //     }
+    // }
 
     // Initialize the UI.
     fn init(
         handle: Self::Init,
-        root: Self::Root,
+        window: Self::Root,
         sender: ComponentSender<Self>,
     ) -> ComponentParts<Self> {
+        window.init_layer_shell();
+        window.set_layer(Layer::Overlay);
+
+        let anchors = [
+            (Edge::Left, true),
+            (Edge::Right, true),
+            (Edge::Top, false),
+            (Edge::Bottom, true),
+        ];
+
+        for (anchor, state) in anchors {
+            window.set_anchor(anchor, state);
+        }
+
         let model = UIModel {
             keyboard_handle: handle,
         };
 
-        // Insert the macro code generation here
-        let widgets = view_output!();
+        let vbox = gtk::Box::builder()
+            .orientation(gtk::Orientation::Horizontal)
+            .spacing(5)
+            .build();
+
+        let inc_button = gtk::Button::with_label("Press");
+        let dec_button = gtk::Button::with_label("Release");
+
+        window.set_child(Some(&vbox));
+        vbox.set_margin_all(5);
+        vbox.set_align(gtk::Align::Center);
+        vbox.append(&inc_button);
+        vbox.append(&dec_button);
+
+        let inc_sender = sender.clone();
+        inc_button.connect_clicked(move |_| {
+            inc_sender.input(UIMessage::Press);
+        });
+
+        let dec_sender = sender.clone();
+        dec_button.connect_clicked(move |_| {
+            dec_sender.input(UIMessage::Release);
+        });
+
+        let widgets = ();
 
         ComponentParts { model, widgets }
     }
@@ -79,4 +127,6 @@ impl SimpleComponent for UIModel {
             }
         }
     }
+
+    fn update_view(&self, _widgets: &mut Self::Widgets, _sender: ComponentSender<Self>) {}
 }
