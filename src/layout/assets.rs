67312c36cbf140fcd73f31917ceec7081e48322d
Tai Zeming <tai.zeming@pithos.net>
2024-09-29 21:50:30 +0800

    chore: correct type

--- src/layout/assets.rs
+++ src/layout/assets.rs
@@ -11,7 +11,7 @@ pub struct LayoutAssets;
 impl LayoutAssets {
     pub fn get_default_60_percent_layout() -> LayoutDefinition {
         // Self-hosted assets must success
-        let layout_file = LayoutAssets::get("defaut-60%.toml").unwrap();
+        let layout_file = LayoutAssets::get("default-60%.toml").unwrap();
         let toml_str = str::from_utf8(layout_file.data.as_ref()).unwrap();
         LayoutDefinition::from_toml(toml_str)
     }
