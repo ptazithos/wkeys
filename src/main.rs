3a2cb7fa090d7b792d3fa04f7f136605fd9610b8
Tai Zeming <tai.zeming@pithos.net>
2024-11-06 16:41:35 +0800

    feat: detect multiple instance

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod ipc;
 mod layout;
 mod native;
 mod service;
@@ -6,17 +7,25 @@ mod ui;
 use layout::assets::LayoutAssets;
 use native::VirtualKeyboard;
 use service::AppService;
+use single_instance::SingleInstance;
+use tracing::info;
 
 fn main() {
     tracing_subscriber::fmt::init();
 
-    let default_layout = LayoutAssets::get_default_60_percent_layout();
+    let instance = SingleInstance::new("net.pithos.wkeys").unwrap();
 
-    let keyboard = VirtualKeyboard::new();
+    if instance.is_single() {
+        let default_layout = LayoutAssets::get_default_60_percent_layout();
 
-    // keyboard.key_press(evdev::Key::KEY_A);
-    // keyboard.key_release(evdev::Key::KEY_A);
+        let keyboard = VirtualKeyboard::new();
 
-    let app_service = AppService::new(keyboard, default_layout);
-    app_service.run();
+        // keyboard.key_press(evdev::Key::KEY_A);
+        // keyboard.key_release(evdev::Key::KEY_A);
+
+        let app_service = AppService::new(keyboard, default_layout);
+        app_service.run();
+    } else {
+        info!("Another instance is already running.");
+    }
 }
