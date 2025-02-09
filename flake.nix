319156933641a3c98be4bb1c5b6aa431f87ebfe3
Yadobler <49450747+yadobler@users.noreply.github.com>
2025-02-09 20:09:26 +0800

    Add lokc

--- flake.nix
+++ flake.nix
@@ -3,26 +3,35 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, nixpkgs }: 
-    let 
-      system = builtins.currentSystem;
-      pkgs = import nixpkgs {};
-      wkeys_build = pkgs.rustPlatform.buildRustPackage {
-        pname = "wkeys";
-        version = "v0.1.0";
+  outputs = { self, nixpkgs, flake-parts}@inputs: 
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
 
-        src = ./.;
-        cargoLock = {
-          lockFile = ./Cargo.lock;
+      perSystem = {pkgs, system, ...}:
+        let 
+          wkeys_build = pkgs.rustPlatform.buildRustPackage {
+            pname = "wkeys";
+            version = "v0.1.0";
+
+            src = ./.;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+
+            nativeBuildInputs = with pkgs; [pkg-config cargo];
+            buildInputs = with pkgs; [gtk4-layer-shell gtkmm4 libxkbcommon];
+          };
+        in 
+          {
+          packages.default = wkeys_build;
         };
-
-        nativeBuildInputs = with pkgs; [pkg-config cargo];
-        buildInputs = with pkgs; [gtk4-layer-shell gtkmm4 libxkbcommon];
-      };
-    in 
-      {
-      pkgs.${system}.default = wkeys_build;
     };
 }
