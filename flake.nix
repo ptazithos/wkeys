ea81979f1eacd9a86dbb7004c02c913ae1039c19
yukna <49450747+yadobler@users.noreply.github.com>
2025-02-09 20:36:11 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A very basic flake for wkeys";
+  description = "A very basic flake for wkeys, by Yadobler";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
